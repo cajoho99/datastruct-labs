0d40bfabbaa84d9b60f111304bde1b8af668c060
Megavex <42978006+Megavex@users.noreply.github.com>
2019-12-03 11:54:42 +0100

    LAB2

--- Autocomplete/src/Term.java
+++ Autocomplete/src/Term.java
@@ -6,12 +6,12 @@ public class Term {
     private long weight;
 
     // Initializes a term with the given query string and weight.
-    public Term(String query, long weight){
+    public Term(String query, long weight) {
 
-        if(query == null)
+        if (query == null)
             throw new NullPointerException("Query is null");
 
-        if(weight < 0)
+        if (weight < 0)
             throw new IllegalArgumentException("Weight can not be negative: " + weight);
 
         this.query = query;
@@ -20,25 +20,36 @@ public class Term {
     }
 
     // Compares the two terms in lexicographic order by query.
-    public static Comparator<Term> byLexicographicOrder(){
+    public static Comparator<Term> byLexicographicOrder() {
         return Comparator.comparing(o -> o.query);
     }
 
     // Compares the two terms in descending order by weight.
-    public static Comparator<Term> byReverseWeightOrder(){
+    public static Comparator<Term> byReverseWeightOrder() {
         return Comparator.comparingLong(o -> -o.weight);
     }
 
     // Compares the two terms in lexicographic order,
     // but using only the first k characters of each query.
-    public static Comparator<Term> byPrefixOrder(int k){
+    public static Comparator<Term> byPrefixOrder(int k) {
 
-        if(k < 0)
+        if (k < 0)
             throw new IllegalArgumentException("k can not be negative: " + k);
 
         return (o1, o2) -> {
-            String o1Substring = o1.query.substring(0, k);
-            String o2Substring = o2.query.substring(0, k);
+            String o1Substring;
+            String o2Substring;
+            if (o1.query.length() < k) {
+                o1Substring = o1.query;
+            } else {
+                o1Substring = o1.query.substring(0, k);
+            }
+
+            if (o2.query.length() < k) {
+                o2Substring = o2.query;
+            } else {
+                o2Substring = o2.query.substring(0, k);
+            }
 
             return o1Substring.compareTo(o2Substring);
         };
